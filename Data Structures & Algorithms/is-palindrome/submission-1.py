578e604d3d4419a9927961086e49b51580fda820
Nailson Landim <nailsonlinux@gmail.com>
2026-05-01 16:27:19 -0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,13 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Sanitize the string
+        s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        # Set the Left/Right pointer
+        L, R = 0, len(s) - 1
+        while L < R:
+            if s[L] != s[R]:
+                return False
+            L += 1
+            R -= 1
+        return True
